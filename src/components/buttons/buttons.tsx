f2efe3c490ab5429824d10e97979694a4de11dd4
MAZE <remvze@proton.me>
2023-10-11 23:05:13 +0330

    feat: add simple tooltip

--- src/components/buttons/buttons.tsx
+++ src/components/buttons/buttons.tsx
@@ -1,5 +1,17 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { BiPause, BiPlay, BiUndo, BiTrash } from 'react-icons/bi/index';
+import {
+  useFloating,
+  autoUpdate,
+  offset,
+  flip,
+  shift,
+  useHover,
+  useFocus,
+  useDismiss,
+  useRole,
+  useInteractions,
+} from '@floating-ui/react';
 
 import { useSoundStore } from '@/store';
 import { usePlay } from '@/contexts/play';
@@ -8,6 +20,34 @@ import { cn } from '@/helpers/styles';
 import styles from './buttons.module.css';
 
 export function Buttons() {
+  /**
+   * Tooltip Start
+   */
+  const [isTooltipOpen, setIsTooltipOpen] = useState(false);
+
+  const { context, floatingStyles, refs } = useFloating({
+    middleware: [offset(15), flip(), shift()],
+    onOpenChange: setIsTooltipOpen,
+    open: isTooltipOpen,
+    placement: 'top',
+    whileElementsMounted: autoUpdate,
+  });
+
+  const hover = useHover(context, { move: false });
+  const focus = useFocus(context);
+  const dismiss = useDismiss(context);
+  const role = useRole(context, { role: 'tooltip' });
+
+  const { getFloatingProps, getReferenceProps } = useInteractions([
+    hover,
+    focus,
+    dismiss,
+    role,
+  ]);
+  /**
+   * Tooltip End
+   */
+
   const { isPlaying, pause, toggle } = usePlay();
   const noSelected = useSoundStore(state => state.noSelected());
   const restoreHistory = useSoundStore(state => state.restoreHistory);
@@ -50,6 +90,8 @@ export function Buttons() {
 
       <button
         disabled={noSelected && !hasHistory}
+        ref={refs.setReference}
+        {...getReferenceProps}
         aria-label={
           hasHistory ? 'Restore Unselected Sounds' : 'Unselect All Sounds'
         }
@@ -65,6 +107,16 @@ export function Buttons() {
       >
         {hasHistory ? <BiUndo /> : <BiTrash />}
       </button>
+
+      {isTooltipOpen && (
+        <div
+          ref={refs.setFloating}
+          style={floatingStyles}
+          {...getFloatingProps({ className: styles.tooltip })}
+        >
+          {hasHistory ? 'Restore unselected sounds.' : 'Unselect all sounds.'}
+        </div>
+      )}
     </div>
   );
 }
