735d9ebebfa36dd3e7596e70a0549b24b7b9fc4d
MAZE <remvze@proton.me>
2023-10-31 13:26:39 +0330

    feat: add Howler for sounds

--- src/hooks/use-sound.ts
+++ src/hooks/use-sound.ts
@@ -1,32 +1,50 @@
-import { useMemo, useEffect } from 'react';
+import { useMemo, useEffect, useCallback } from 'react';
+import { Howl } from 'howler';
 
 import { useSSR } from './use-ssr';
 
 export function useSound(
   src: string,
   options: { loop?: boolean; volume?: number } = {},
-): HTMLAudioElement | null {
+) {
   const { isBrowser } = useSSR();
-  const sound = useMemo<HTMLAudioElement | null>(() => {
-    let sound: HTMLAudioElement | null = null;
+  const sound = useMemo<Howl | null>(() => {
+    let sound: Howl | null = null;
 
     if (isBrowser) {
-      sound = new Audio(src);
-      sound.preload = 'none';
+      sound = new Howl({ preload: false, src: src });
     }
 
     return sound;
   }, [src, isBrowser]);
 
   useEffect(() => {
-    if (sound)
-      sound.loop = typeof options.loop === 'boolean' ? options.loop : false;
+    if (sound) {
+      sound.loop(typeof options.loop === 'boolean' ? options.loop : false);
+    }
   }, [sound, options.loop]);
 
   useEffect(() => {
     if (sound)
-      sound.volume = typeof options.volume === 'number' ? options.volume : 0.5;
+      sound.volume(typeof options.volume === 'number' ? options.volume : 0.5);
   }, [sound, options.volume]);
 
-  return sound;
+  const play = useCallback(() => {
+    if (sound) {
+      sound.load();
+      sound.play();
+    }
+  }, [sound]);
+
+  const stop = useCallback(() => {
+    if (sound) sound.stop();
+  }, [sound]);
+
+  const pause = useCallback(() => {
+    if (sound) sound.pause();
+  }, [sound]);
+
+  const control = useMemo(() => ({ pause, play, stop }), [play, stop, pause]);
+
+  return control;
 }
