eed5a1329d6fc36d1e6375feaeaf2bba26167bf5
MAZE <remvze@proton.me>
2023-12-07 20:46:57 +0330

    chore: add more sounds

--- src/data/sounds/nature.tsx
+++ src/data/sounds/nature.tsx
@@ -2,7 +2,7 @@ import { GiWaterfall } from 'react-icons/gi/index';
 import { BsFire } from 'react-icons/bs/index';
 import { BiSolidTree, BiWater } from 'react-icons/bi/index';
 import { FaWater, FaWind, FaLeaf, FaRegSnowflake } from 'react-icons/fa/index';
-import { TbScubaMask } from 'react-icons/tb/index';
+import { TbScubaMask, TbSailboat } from 'react-icons/tb/index';
 
 import type { Category } from '../types';
 
@@ -69,6 +69,12 @@ export const nature: Category = {
       id: 'walk-on-leaves',
       label: 'Walk on Leaves',
       src: '/sounds/nature/walk-on-leaves.mp3',
+    },
+    {
+      icon: <TbSailboat />,
+      id: 'rowing-boat',
+      label: 'Rowing Boat',
+      src: '/sounds/nature/rowing-boat.mp3',
     },
   ],
   title: 'Nature',
