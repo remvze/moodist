ff26597d22d444d18d2874a5c278eccc288972de
MAZE <remvze@proton.me>
2024-01-04 19:51:58 +0330

    feat: add disabled state

--- src/components/menu/item/item.tsx
+++ src/components/menu/item/item.tsx
@@ -1,14 +1,15 @@
 import styles from './item.module.css';
 
 interface ItemProps {
+  disabled: boolean;
   icon: React.ReactElement;
   label: string;
   onClick: () => void;
 }
 
-export function Item({ icon, label, onClick }: ItemProps) {
+export function Item({ disabled = false, icon, label, onClick }: ItemProps) {
   return (
-    <button className={styles.item} onClick={onClick}>
+    <button className={styles.item} disabled={disabled} onClick={onClick}>
       <span className={styles.icon}>{icon}</span> {label}
     </button>
   );
--- src/components/menu/items/share.tsx
+++ src/components/menu/items/share.tsx
@@ -2,12 +2,21 @@ import { IoShareSocialSharp } from 'react-icons/io5/index';
 
 import { Item } from '../item';
 
+import { useSoundStore } from '@/store';
+
 interface ShareProps {
   open: () => void;
 }
 
 export function Share({ open }: ShareProps) {
+  const noSelected = useSoundStore(state => state.noSelected());
+
   return (
-    <Item icon={<IoShareSocialSharp />} label="Share Sounds" onClick={open} />
+    <Item
+      disabled={noSelected}
+      icon={<IoShareSocialSharp />}
+      label="Share Sounds"
+      onClick={open}
+    />
   );
 }
