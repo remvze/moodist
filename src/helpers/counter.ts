ae3ea8c74f9a94ae56a0eb4b165bc36d990dea7b
MAZE <remvze@proton.me>
2024-02-24 16:33:41 +0330

    fix: remove word counter dependency

--- src/helpers/counter.ts
+++ src/helpers/counter.ts
@@ -1,10 +1,11 @@
-import wordsCounter from 'word-counting';
-
 export function count(_string: string) {
   const string = _string.trim();
 
   return {
     characters: string.replace(/\s/g, '').length,
-    words: wordsCounter(string).wordsCount,
+    words: string
+      .replace(/\n/g, ' ')
+      .split(' ')
+      .filter(str => str !== '').length,
   };
 }
