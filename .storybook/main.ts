0300df3852838135245882a8aa1c59dd1a3f8af7
MAZE <remvze@proton.me>
2024-04-27 22:49:48 +0330

    chore: add accessibility addon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -11,6 +11,7 @@ const config: StorybookConfig = {
     '@storybook/addon-essentials',
     '@chromatic-com/storybook',
     '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
   ],
 
   framework: {
