f79e941527e09e96b5eba6ca8c4e2e3df583c071
MAZE <remvze@proton.me>
2024-05-11 15:30:49 +0430

    feat: add media session (wip)

--- src/hooks/use-media-session.ts
+++ src/hooks/use-media-session.ts
@@ -8,11 +8,16 @@ export function useMediaSession() {
   useEffect(() => {
     if ('mediaSession' in navigator) {
       if (isPlaying) {
+        console.log('hello');
         navigator.mediaSession.metadata = new MediaMetadata({
           title: 'Moodist - Ambient Sounds',
         });
+
+        navigator.mediaSession.setActionHandler('play', function () {});
+        navigator.mediaSession.setActionHandler('pause', function () {});
       } else {
         navigator.mediaSession.metadata = null;
+        console.log('bye');
       }
     }
   }, [isPlaying]);
