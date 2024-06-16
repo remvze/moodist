9d1d8f80359097b9122673564d3d57c0827ff3db
MAZE <remvze@proton.me>
2024-06-16 18:51:23 +0330

    style: change notice

--- src/components/toolbox/countdown-timer/timers/notice/notice.tsx
+++ src/components/toolbox/countdown-timer/timers/notice/notice.tsx
@@ -3,8 +3,8 @@ import styles from './notice.module.css';
 export function Notice() {
   return (
     <p className={styles.notice}>
-      Please do not close this tab while timers are running, otherwise all
-      timers will be stopped.
+      Please do not close your browser tab while timers are running, otherwise
+      all timers will be stopped.
     </p>
   );
 }
