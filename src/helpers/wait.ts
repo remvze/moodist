9cc0ccd325cf769d64779f133bd2d59e6ba7ca58
MAZE <remvze@proton.me>
2024-06-19 14:23:27 +0430

    test: write more tests

--- src/helpers/wait.ts
+++ src/helpers/wait.ts
@@ -1,3 +1,10 @@
+/**
+ * Repeatedly calls a function at a specified interval until it returns `true`.
+ *
+ * @param {() => boolean} func - A function that returns a boolean. The interval will continue until this function returns `true`.
+ * @param {number} interval - The time, in milliseconds, between each call to `func`.
+ * @returns {Promise<void>} A promise that resolves when `func` returns `true`, or rejects if an error is thrown during execution of `func`.
+ */
 export function waitUntil(
   func: () => boolean,
   interval: number,
