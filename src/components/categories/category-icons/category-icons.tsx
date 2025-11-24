04c52962c3b65ebb7875ebadf20132846a5c020b
MAZE <remvze@proton.me>
2025-11-25 00:10:07 +0330

    style: minor changes

--- src/components/categories/category-icons/category-icons.tsx
+++ src/components/categories/category-icons/category-icons.tsx
@@ -12,17 +12,17 @@ export default function CategoryIcons() {
       <div className={styles.wrapper}>
         <h3 className={styles.title}>Categories</h3>
         <div className={styles.categoryIconsWrapper}>
-          {categories.map(category => {
-            return (
-              <a href={`#category-${category.id}`} key={category.id}>
-                <Tooltip.Provider delayDuration={0}>
+          <Tooltip.Provider delayDuration={0}>
+            {categories.map(category => {
+              return (
+                <a href={`#category-${category.id}`} key={category.id}>
                   <Tooltip content={category.title} placement="bottom">
                     <div className={styles.icon}>{category.icon}</div>
                   </Tooltip>
-                </Tooltip.Provider>
-              </a>
-            );
-          })}
+                </a>
+              );
+            })}
+          </Tooltip.Provider>
         </div>
       </div>
     </Container>
