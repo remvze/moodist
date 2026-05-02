0e49a2c80fe11bfd938cf93aeceea440e19f6668
MAZE <remvze@proton.me>
2026-05-02 23:07:36 +0330

    refactor: flatten and inline stores

---
+++ src/stores/note.ts
@@ -0,0 +1,61 @@
+import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
+import merge from 'deepmerge';
+
+import { count } from '@/helpers/counter';
+
+interface NoteStore {
+  characters: () => number;
+  clear: () => void;
+  history: string | null;
+  note: string;
+  restore: () => void;
+  words: () => number;
+  write: (note: string) => void;
+}
+
+export const useNoteStore = create<NoteStore>()(
+  persist(
+    (set, get) => ({
+      characters() {
+        return count(get().note).characters;
+      },
+
+      clear() {
+        if (!get().note) return;
+
+        set({ history: get().note, note: '' });
+      },
+
+      history: null,
+      note: '',
+
+      restore() {
+        if (!get().history) return;
+
+        set({ history: null, note: get().history! });
+      },
+
+      words() {
+        return count(get().note).words;
+      },
+
+      write(note) {
+        set({ history: null, note });
+      },
+    }),
+    {
+      merge: (persisted, current) =>
+        merge(
+          current,
+          // @ts-ignore
+          persisted,
+        ),
+      name: 'moodist-note',
+      partialize: state => ({ note: state.note }),
+      skipHydration: true,
+      storage: createJSONStorage(() => localStorage),
+      version: 0,
+    },
+  ),
+);
---
+++ src/stores/sound.ts
@@ -0,0 +1,228 @@
+import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
+import merge from 'deepmerge';
+
+import { sounds as soundCategories } from '@/data/sounds';
+import { pickMany, random } from '@/helpers/random';
+
+type SoundValue = {
+  isFavorite: boolean;
+  isSelected: boolean;
+  volume: number;
+};
+
+interface SoundStore {
+  getFavorites: () => Array<string>;
+  globalVolume: number;
+  history: Record<string, SoundValue> | null;
+  isPlaying: boolean;
+  lock: () => void;
+  locked: boolean;
+  noSelected: () => boolean;
+  override: (sounds: Record<string, number>) => void;
+  pause: () => void;
+  play: () => void;
+  restoreHistory: () => void;
+  select: (id: string) => void;
+  setGlobalVolume: (volume: number) => void;
+  setVolume: (id: string, volume: number) => void;
+  shuffle: () => void;
+  sounds: Record<string, SoundValue>;
+  toggleFavorite: (id: string) => void;
+  togglePlay: () => void;
+  unlock: () => void;
+  unselect: (id: string) => void;
+  unselectAll: (pushToHistory?: boolean) => void;
+}
+
+function createInitialSounds() {
+  const initialSounds: Record<string, SoundValue> = {};
+
+  soundCategories.categories.forEach(category => {
+    category.sounds.forEach(sound => {
+      initialSounds[sound.id] = {
+        isFavorite: false,
+        isSelected: false,
+        volume: 0.5,
+      };
+    });
+  });
+
+  return initialSounds;
+}
+
+export const useSoundStore = create<SoundStore>()(
+  persist(
+    (set, get) => ({
+      getFavorites() {
+        const { sounds } = get();
+        const ids = Object.keys(sounds);
+        const favorites = ids.filter(id => sounds[id].isFavorite);
+
+        return favorites;
+      },
+
+      globalVolume: 1,
+      history: null,
+      isPlaying: false,
+
+      lock() {
+        set({ locked: true });
+      },
+
+      locked: false,
+
+      noSelected() {
+        const { sounds } = get();
+        const keys = Object.keys(sounds);
+
+        return keys.every(key => !sounds[key].isSelected);
+      },
+
+      override(newSounds) {
+        get().unselectAll();
+
+        const sounds = get().sounds;
+
+        Object.keys(newSounds).forEach(sound => {
+          if (sounds[sound]) {
+            sounds[sound].isSelected = true;
+            sounds[sound].volume = newSounds[sound];
+          }
+        });
+
+        set({ history: null, sounds: { ...sounds } });
+      },
+
+      pause() {
+        set({ isPlaying: false });
+      },
+
+      play() {
+        set({ isPlaying: true });
+      },
+
+      restoreHistory() {
+        const history = get().history;
+
+        if (!history) return;
+
+        set({ history: null, sounds: history });
+      },
+
+      select(id) {
+        set({
+          history: null,
+          sounds: {
+            ...get().sounds,
+            [id]: { ...get().sounds[id], isSelected: true },
+          },
+        });
+      },
+
+      setGlobalVolume(volume) {
+        set({
+          globalVolume: volume,
+        });
+      },
+
+      setVolume(id, volume) {
+        set({
+          sounds: {
+            ...get().sounds,
+            [id]: { ...get().sounds[id], volume },
+          },
+        });
+      },
+
+      shuffle() {
+        const sounds = get().sounds;
+        const ids = Object.keys(sounds);
+
+        ids.forEach(id => {
+          sounds[id].isSelected = false;
+          sounds[id].volume = 0.5;
+        });
+
+        const randomIDs = pickMany(ids, 4);
+
+        randomIDs.forEach(id => {
+          sounds[id].isSelected = true;
+          sounds[id].volume = random(0.2, 1);
+        });
+
+        set({ history: null, isPlaying: true, sounds });
+      },
+
+      sounds: createInitialSounds(),
+
+      toggleFavorite(id) {
+        const sounds = get().sounds;
+        const sound = sounds[id];
+
+        set({
+          history: null,
+          sounds: {
+            ...sounds,
+            [id]: { ...sound, isFavorite: !sound.isFavorite },
+          },
+        });
+      },
+
+      togglePlay() {
+        set({ isPlaying: !get().isPlaying });
+      },
+
+      unlock() {
+        set({ locked: false });
+      },
+
+      unselect(id) {
+        set({
+          sounds: {
+            ...get().sounds,
+            [id]: { ...get().sounds[id], isSelected: false },
+          },
+        });
+      },
+
+      unselectAll(pushToHistory = false) {
+        const noSelected = get().noSelected();
+
+        if (noSelected) return;
+
+        const sounds = get().sounds;
+
+        if (pushToHistory) {
+          const history = JSON.parse(JSON.stringify(sounds));
+          set({ history });
+        }
+
+        const ids = Object.keys(sounds);
+
+        ids.forEach(id => {
+          sounds[id].isSelected = false;
+          sounds[id].volume = 0.5;
+        });
+
+        set({ sounds });
+      },
+    }),
+    {
+      merge: (persisted, current) =>
+        merge(
+          current,
+          // @ts-ignore
+          persisted,
+        ),
+      name: 'moodist-sounds',
+      partialize: state => ({
+        globalVolume: state.globalVolume,
+        sounds: state.sounds,
+      }),
+      skipHydration: true,
+      storage: createJSONStorage(() => localStorage),
+      version: 0,
+    },
+  ),
+);
